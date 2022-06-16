b1193beb4700e1620e122cf2fc65af46174d2b19
joebrentnall <joe.brentnall25@gmail.com>
2022-06-16 23:57:34 +0100

    Added areas component tests

--- src/App.test.js
+++ src/App.test.js
@@ -4,30 +4,46 @@ import { shallow } from 'enzyme';
 import Bookings from './components/BookTable/Bookings';
 import { typeData } from "./Assets/data/data";
 
-test('renders learn react link', () => {
-  expect(true).toBeTruthy();
-});
+import Areas from "./components/Areas/Areas";
 
 describe("Test booking component", () => {
   test("test table booking", () => {
-    const comp = shallow(<Bookings type="table"/>);
-    const header = comp.find('.table-booking__header');
-    const text = comp.find('.table-booking__info-text');
+    const component = shallow(<Bookings type="table"/>);
+    const header = component.find('.table-booking__header');
+    const text = component.find('.table-booking__info-text');
+    
     expect(header.text()).toBe(typeData.table.header);
     expect(text.text()).toBe(typeData.table.text);
   })
   test("test event booking", () => {
-    const comp = shallow(<Bookings type="event"/>);
-    const header = comp.find('.table-booking__header');
-    const text = comp.find('.table-booking__info-text');
+    const component = shallow(<Bookings type="event"/>);
+    const header = component.find('.table-booking__header');
+    const text = component.find('.table-booking__info-text');
+
     expect(header.text()).toBe(typeData.event.header);
     expect(text.text()).toBe(typeData.event.text);
   })
   test("test undefined booking", () => {
-    const comp = shallow(<Bookings type="test"/>);
-    const header = comp.find('.table-booking__header');
-    const text = comp.find('.table-booking__info-text');
+    const component = shallow(<Bookings type="test"/>);
+    const header = component.find('.table-booking__header');
+    const text = component.find('.table-booking__info-text');
+
     expect(header.text()).toBe(typeData.default.header);
     expect(text.text()).toBe(typeData.default.text);
+  })
+})
+
+describe("Test areas component", () => {
+  test("test areas has data", () => {
+    const component = shallow(<Areas/>)
+    const name = component.find(".areas__main-content-title");
+    const description = component.find(".areas__main-content-para");
+    const link = component.find(".areas__main-content-link");
+    const imageLink = component.find(".areas__main-img");
+
+    expect(name.text().length).toBeGreaterThan(0);
+    expect(description.text().length).toBeGreaterThan(0);
+    expect(link.text().length).toBeGreaterThan(0);
+    expect(imageLink.text().length).toBeGreaterThan(0);
   })
 })
