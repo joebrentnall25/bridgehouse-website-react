530dffd67924a28bfeb97e7baa435ec4cbf61e59
Joe Brentnall <joe.brentnall25@gmail.com>
2022-06-18 15:45:38 +0100

    Added reviews component tests

--- src/App.test.js
+++ src/App.test.js
@@ -52,7 +52,7 @@ describe("Test booking component", () => {
     expect(header.text()).toBe(typeData.event.header);
     expect(text.text()).toBe(typeData.event.text);
   })
-  test("test undefined booking", () => {
+  test("test invalid booking", () => {
     const component = shallow(<Bookings type="test"/>);
     const header = component.find('.table-booking__header');
     const text = component.find('.table-booking__info-text');
@@ -78,18 +78,18 @@ describe("Test areas component", () => {
 })
 
 describe("Test reviews component", () => {
-  const component = shallow(<Reviews/>);
   test("test reviews has data", () => {
+    const component = shallow(<Reviews/>);
     const reviewName = component.find(".reviews__name");
     const reviewText = component.find(".reviews__para");
     expect(reviewName.text().length).toBeGreaterThan(0);
     expect(reviewText.text().length).toBeGreaterThan(0);
   })
-  test("test button component used in reviews", () => {
-    const button = component.find(Button)
-    expect(button[0].instance()).toBeCalledWith({
-      type: "simple",
-      text: "Previous"
-    });
+})
+
+describe("Test buttons component", () => {
+  test.skip("test primary button", () => {
+    const component = shallow(<Button/>);
   })
 })
+
