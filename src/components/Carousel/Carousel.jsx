3c93324500b27a79c39b72f32a281c668d0eeb5a
Joe Brentnall <joe.brentnall25@gmail.com>
2024-08-20 22:00:08 +0100

    Carousel refactor

--- src/components/Carousel/Carousel.jsx
+++ src/components/Carousel/Carousel.jsx
@@ -11,30 +11,18 @@ const Carousel = (props) => {
     useEffect(() => {
         setImageIndex(0);
     },[currentArea])
-    
-    const nextImage = () => {
-        if (imageIndex === currentArea.imgLink.length-1){
-            setImageIndex(0);
-        } else {
-            setImageIndex(imageIndex+1);
-        }
-        console.log(imageIndex);
-    }
 
-    const previousImage = () => {
-        if (imageIndex === 0) {
-            setImageIndex(currentArea.imgLink.length-1);
-        } else {
-            setImageIndex(imageIndex-1);
-        }
-        console.log(imageIndex);
+    const changeImage = changeAmount => {
+        if (imageIndex + changeAmount > currentArea.imgLink.length-1) setImageIndex(0)
+        else if (imageIndex + changeAmount < 0) setImageIndex(currentArea.imgLink.length-1)
+        else setImageIndex(imageIndex + changeAmount)
     }
 
     return (     
     <div className="carousel">
         <img className="carousel__img" src={currentArea.imgLink[imageIndex]} alt=""/>
-        <button className="carousel__button carousel__button--prev" onClick={() => previousImage()}><BsArrowLeftCircleFill size={28}/></button>
-        <button className="carousel__button carousel__button--next" onClick={() => nextImage()}><BsArrowRightCircleFill size={28}/></button>
+        <button className="carousel__button carousel__button--prev" onClick={() => changeImage(-1)}><BsArrowLeftCircleFill size={28}/></button>
+        <button className="carousel__button carousel__button--next" onClick={() => changeImage(1)}><BsArrowRightCircleFill size={28}/></button>
         <p className="carousel__counter">{imageIndex+1}/{maxIndex}</p>
     </div>)
 }
